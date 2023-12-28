1444b3d74775cd720a6e8a88a077d01a350abb67
Gonzalez Tenreiro <jgonzalezt@santiagodecompostela.gal>
2023-12-28 14:44:12 +0100

    Investigando props en Astro y librerías de svg para tailwind

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Badge } from './Badge.astro'
+export { default as SocialLink} from './SocialLink.astro'
+export { default as TheCard } from './TheCard.astro'
+export { default as TheHeader } from './TheHeader.astro'
