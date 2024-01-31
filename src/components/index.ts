9b5280ebdaa442234b097180840acd6db509ae86
webferrol <xurxo@webferrol.com>
2024-01-31 14:07:42 +0100

    Alias created

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as AboutMe } from './sections/AboutMe.astro'
-export { default as Experience } from './Experience.astro'
-export { default as Markdown } from './sections/Markdown.astro'
-export { default as SocialLink} from './ui/SocialLink.astro'
-export { default as TheCard } from './TheCard.astro'
