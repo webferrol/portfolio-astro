50c0f961d90cce0d794ec48fde299ce737e7fb23
webferrol <xurxo@webferrol.com>
2024-02-09 14:43:27 +0100

    added label to select

--- src/components/sections/Bento/HTMLSearchElement.tsx
+++ src/components/sections/Bento/HTMLSearchElement.tsx
@@ -1,7 +1,7 @@
 export function HTMLSearchElement ({ tags, onSearch } : {tags: string[], onSearch: (event: Event) => void}) {
     
   return (
-          <div role="search" className="max-w-[1140px]  mx-auto mb-2">
+          <label aria-label="Selecciona una categoría" role="search" className="block max-w-[1140px]  mx-auto mb-2">
               <select className="bg-bg dark:bg-bg-dark py-1 px-2 border-solid border-2 rounded" onChange={onSearch}>
                   <option value="">Todo</option>
                   {
@@ -10,6 +10,6 @@ export function HTMLSearchElement ({ tags, onSearch } : {tags: string[], onSearc
                       ))
                   }
               </select>
-          </div>
+          </label>
   )
 }
--- src/components/ui/HTMLSelect.tsx
+++ src/components/ui/HTMLSelect.tsx
@@ -29,18 +29,20 @@ export default function HTMLSearchElement ({ tags } : {tags: string[] }) {
 
     return (
             <form ref={formRef} action='/tags' role="search" className="max-w-[1140px]  mx-auto mb-2">
-                <select
-                    placeholder="Categorías"
-                    onChange={handleChange} 
-                    className="bg-bg dark:bg-bg-dark py-1 px-2 border-solid border-2 rounded">
-                    <option>Categoría</option>
-                    <option value="">Todo</option>
-                    {
-                        tags.map(item => (
-                            <option value={item} key={item}>{item}</option>
-                        ))
-                    }
-                </select>
+                <label aria-label="Selecciona una categoría">
+                    <select
+                        placeholder="Categorías"
+                        onChange={handleChange}
+                        className="bg-bg dark:bg-bg-dark py-1 px-2 border-solid border-2 rounded">
+                        <option>Categoría</option>
+                        <option value="">Todo</option>
+                        {
+                            tags.map(item => (
+                                <option value={item} key={item}>{item}</option>
+                            ))
+                        }
+                    </select>
+                </label>
             </form>
     )
   }
