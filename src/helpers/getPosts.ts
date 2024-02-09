d45960d1f9e2386787fc33f0c61d623bfc99d450
webferrol <xurxo@webferrol.com>
2024-02-09 11:30:10 +0100

    example for fiter data with getCollection

--- src/helpers/getPosts.ts
+++ src/helpers/getPosts.ts
@@ -2,6 +2,11 @@ import { getCollection } from 'astro:content'
 
 const posts = await getCollection('posts')
 
+// Filtrado
+// const posts = await getCollection('posts', ({data}) => {
+//   return data.tags.includes('arrays') // Ejemplo para buscar con palabras clave
+// })
+
 let sortedPosts = posts.sort((a, b) => {
   return  Number(b.data.pubDate) - Number(a.data.pubDate)
 })
