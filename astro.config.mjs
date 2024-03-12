d0d95c9051fc84427f35215d7e06e98b8c67ddda
webferrol <xurxo@webferrol.com>
2024-03-12 11:08:35 +0100

    Add astro-compress for files statics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact(), compress()]
   // site: 'https://webferrol.github.io',
   // base: "/portfolio-astro",
   // output: "static"
