478cc6d547aec204f7a50002ff43152c94bda082
webferrol <xurxo@webferrol.com>
2024-03-16 22:46:51 +0100

    Added constant URLBASE in constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,7 @@
-export const WORDPRESS_URL = 'https://www.webferrol.com/wp-json/wp/v2/posts'
+const URLBASE = 'https://webferrol.netlify.app/'
+const WORDPRESS_URL = 'https://www.webferrol.com/wp-json/wp/v2/posts'
+
+export {
+  URLBASE,
+  WORDPRESS_URL
+}
