80b50fe2cc8aaabcda45a33c811701113f86130b
webferrol <xurxo@webferrol.com>
2024-03-17 14:27:00 +0100

    Added constant to store keys entities html to escape them

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,18 @@
 const URLBASE = 'https://webferrol.netlify.app/'
 const WORDPRESS_URL = 'https://www.webferrol.com/wp-json/wp/v2/posts'
+const UNESCAPE_HTML_OBJECT: unescapeHTML = {
+  '&amp;': '&',
+  '&lt;': '<',
+  '&gt;': '>',
+  '&#39;': "'",
+  '&quot;': '"',
+  '&#8221;': '"',
+  '&#8220;': '"',
+  '&#8211;': '-',
+}
 
 export {
+  UNESCAPE_HTML_OBJECT,
   URLBASE,
   WORDPRESS_URL
 }
